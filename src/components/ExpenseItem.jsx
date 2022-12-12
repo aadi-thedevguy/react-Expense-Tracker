168058920970c4fd7e8eb5c583ae453b9b5809a5
Aditya_Khare <86819684+aadi-thedevguy@users.noreply.github.com>
2022-12-12 14:43:43 +0000

    Fat Arrow functions and organised the code better

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,14 @@
-import React from "react";
 
-const ExpenseItem = ({amount,date,title}) => {
-  
+import ExpenseDate from './ExpenseDate'
+import ExpenseDetails from './ExpenseDetails'
+
+const ExpenseItem = ({ amount, date, title, location }) => {
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
-      <div className="expense-item__description">
-         <h2>{title}</h2>
-        <div className="expense-item__price">{amount}</div>
-      </div>
+      <ExpenseDate date={date} />
+      <h3> {location}</h3>
+      <ExpenseDetails amount={amount} title={title} />
     </div>
   )
 }
