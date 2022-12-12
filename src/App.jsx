2fe19fb6cb7ab2220ee8764755c8408dbd50cc04
Aditya Khare <>
2022-12-12 13:31:54 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import ExpenseItem from './components/ExpenseItem'
+
+export default function App() {
+  return (
+    <main>
+      <h1>Expense Items</h1>
+      <ExpenseItem />
+    </main>
+  )
+}
